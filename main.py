84dbba5bbd8b18d008604f7b83f4a9ff3edcd981
Josh Vazquez <joshv@sfu.ca>
2017-02-10 00:29:32 -0800

    Improved layout on some tabs. Added image opening from tagging tab image list. Not creating image objects with telemetry yet. Implemented reviewed, previous, next buttons.

--- main.py
+++ main.py
@@ -10,14 +10,52 @@ class Window(QtWidgets.QMainWindow):
         super(Window, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.ui.taggableImage.viewport().installEventFilter(self)
+        self.connectButtons()
+        self.ui.viewer_single.viewport().installEventFilter(self)
+
+    def connectButtons(self):
+        self.ui.list_images.currentItemChanged.connect(self.currentImageChanged)
+        self.ui.button_toggleReviewed.clicked.connect(self.toggleImageReviewed)
+        self.ui.button_previous.clicked.connect(self.previousImage)
+        self.ui.button_next.clicked.connect(self.nextImage)
+
+    def toggleImageReviewed(self):
+        item = self.ui.list_images.currentItem()
+        if item:
+            font = item.font()
+            font.setBold(not font.bold())
+            item.setFont(font)
+
+    def currentImageChanged(self, current, _):
+        viewer = self.ui.viewer_single
+        listName = current.listWidget().objectName()
+        if listName == "list_taggedImages":
+            viewer = self.ui.viewer_targets
+        elif listName == "list_allImages":
+            viewer = self.ui.viewer_map
+
+        path = current.text()
+        self.openImage(path, viewer)
+
+    def openImage(self, path, viewer):
+        viewer.setPhoto(QtGui.QPixmap(path))
+
+    def previousImage(self):
+        self.setImageRow(self.ui.list_images.currentRow() - 1)
+
+    def nextImage(self):
+        self.setImageRow(self.ui.list_images.currentRow() + 1)
+
+    def setImageRow(self, row):
+        if 0 <= row < self.ui.list_images.count():
+            self.ui.list_images.setCurrentRow(row)
 
     def eventFilter(self, source, event):
         if (event.type() == QtCore.QEvent.MouseMove and
-                source is self.ui.taggableImage.viewport()):
+                source is self.ui.viewer_single.viewport()):
             if event.buttons() == QtCore.Qt.NoButton:
-                if not self.ui.taggableImage.isImageNull():
-                    point = self.ui.taggableImage.mapToScene(event.pos())
+                if not self.ui.viewer_single.isImageNull():
+                    point = self.ui.viewer_single.mapToScene(event.pos())
                     self.ui.statusbar.showMessage('x: %d, y: %d' % (round(point.x()), round(point.y())))
 
         return QtWidgets.QWidget.eventFilter(self, source, event)
@@ -27,7 +65,5 @@ if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     window = Window()
     window.show()
-
-    window.ui.taggableImage.setPhoto(QtGui.QPixmap("20160430_111051_217148.jpg"))
 
     sys.exit(app.exec_())
