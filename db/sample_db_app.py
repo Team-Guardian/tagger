e8aa94a15739c6c7e623737d2048d81f8c7f7b2e
Vadim Tsarkov <vtsarkov@sfu.ca>
2017-02-11 22:02:51 -0800

    fix import/setup order is sample app

--- db/sample_db_app.py
+++ db/sample_db_app.py
@@ -2,10 +2,13 @@ import os
 import django
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_settings.settings")
+
 django.setup()
 
+from db.models import *
+
 if __name__=="__main__":
-    print 'success'
+    print 'Debug Point'
 
 
 
