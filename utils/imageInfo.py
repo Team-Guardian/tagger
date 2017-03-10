cc53c33fd909f34133aaa5b4c80341ef5ce2dc0f
Andrey Gromov <agromov@sfu.ca>
2017-03-10 12:26:01 -0800

    WIP: added simple GeoTiff parser

--- utils/imageInfo.py
+++ utils/imageInfo.py
@@ -1,4 +1,6 @@
 from math import degrees
+from osgeo import gdal
+import re
 import pyexiv2
 
 
@@ -12,7 +14,30 @@ def loadExif(img):
     img.pitch, img.roll, img.yaw = [degrees(float(x)) for x in telemetry[3:]]
 
 # load geotiff data and convert image from tiff to png
-def loadGeotiff(img):
-    ul_lat, ul_lon = 49.922492, -98.312083
-    lr_lat, lr_lon = 49.886744, -98.233800
+def loadGeotiff(img_path):
+    metadata = gdal.Info(img_path + '.tif') # read GeoTIFF metadata from the given image in *.tif* format
+    ul, ur, lr, ll = None # create empty fields for the metadata we are searching for
+
+    # find lines containing geo-information in metadata
+    ul = re.match("(Upper Left).+$", metadata, flags=re.MULTILINE)
+    # ur = re.match("(Upper Right).+$", metadata, flags=re.MULTILINE)
+    lr = re.match("(Lower Right).+$", metadata, flags=re.MULTILINE)
+    # ll = re.match("(Lower Left).+$", metadata, flags=re.MULTILINE)
+
+    # parse the strings to get geodetic coordinates in DMS
+    ul_dms_lon = re.findall("(\d+(?=d)|\d+(?=\')|\d+\.\d+(?=\"))(?=.*[W|E])", ul.group(0))
+    ul_dms_lat = re.findall("(\d+(?=d)|\d+(?=\')|\d+\.\d+(?=\"))(?=.*[N|S])", ul.group(0))
+
+    # ur_dms_lon = re.findall("(\d+(?=d)|\d+(?=\')|\d+\.\d+(?=\"))(?=.*[W|E])", ur.group(0))
+    # ur_dms_lat = re.findall("(\d+(?=d)|\d+(?=\')|\d+\.\d+(?=\"))(?=.*[N|S])", ur.group(0))
+
+    lr_dms_lon = re.findall("(\d+(?=d)|\d+(?=\')|\d+\.\d+(?=\"))(?=.*[W|E])", lr.group(0))
+    lr_dms_lat = re.findall("(\d+(?=d)|\d+(?=\')|\d+\.\d+(?=\"))(?=.*[N|S])", lr.group(0))
+
+    # ll_dms_lon = re.findall("(\d+(?=d)|\d+(?=\')|\d+\.\d+(?=\"))(?=.*[W|E])", ll.group(0))
+    # ll_dms_lat = re.findall("(\d+(?=d)|\d+(?=\')|\d+\.\d+(?=\"))(?=.*[N|S])", ll.group(0))
+
     return ul_lat, ul_lon, lr_lat, lr_lon
+
+def convertDmsToDecimal(dms_coords):
+
