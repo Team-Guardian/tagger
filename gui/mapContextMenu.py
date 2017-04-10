f088419b280ed9b06a0ec03227dc88ad74e3ddee
Andrey Gromov <agromov@sfu.ca>
2017-04-10 00:55:14 -0700

    implemented filter function, added reset filter function to context menu

--- gui/mapContextMenu.py
+++ gui/mapContextMenu.py
@@ -8,6 +8,8 @@ class MapContextMenu(QtWidgets.QMenu):
         self.find_images_containing_point = self.addAction("Find images containing this point")
         self.addSeparator()
         self.copy_pixel_lat_lon = self.addAction("Copy latitude/longitude")
+        self.addSeparator()
+        self.reset_all_filters = self.addAction("Reset all filters")
 
         # store pixel position where the context menu was invoked
         self.pixel_x_invocation_coord = None
@@ -17,3 +19,4 @@ class MapContextMenu(QtWidgets.QMenu):
         self.map_action_tuples = []
         self.map_action_tuples.append(("FIND_IMAGES", self.find_images_containing_point))
         self.map_action_tuples.append(("COPY_LAT_LON", self.copy_pixel_lat_lon))
+        self.map_action_tuples.append(("RESET_FILTERS", self.reset_all_filters))
--- gui/mapTab.py
+++ gui/mapTab.py
@@ -22,12 +22,22 @@ class MapTab(QtWidgets.QWidget, Ui_MapTab, Observer):
 
     def notify(self, event, id, data):
         if event is "FIND_IMAGES":
-            pass
+            point_pixel_x_coord = self.viewer_map.getPhotoItem().map_context_menu.pixel_x_invocation_coord
+            point_pixel_y_coord = self.viewer_map.getPhotoItem().map_context_menu.pixel_y_invocation_coord
+            point_lat, point_lon = geolocateLatLonFromPixel(self.currentImage, self.currentFlight.reference_altitude,
+                                                point_pixel_x_coord, point_pixel_y_coord)
+            self.line_latitude.setText('{}'.format(point_lat))
+            self.line_longitude.setText('{}'.format(point_lon))
+            self.search()
         elif event is "COPY_LAT_LON":
             point_pixel_x_coord = self.viewer_map.getPhotoItem().map_context_menu.pixel_x_invocation_coord
             point_pixel_y_coord = self.viewer_map.getPhotoItem().map_context_menu.pixel_y_invocation_coord
             lat, lon = geolocateLatLonFromPixel(self.currentImage, self.currentFlight.reference_altitude, point_pixel_x_coord, point_pixel_y_coord)
             QtWidgets.QApplication.clipboard().setText('{}, {}'.format(lat, lon))
+        elif event is "RESET_FILTERS":
+            self.line_latitude.setText('')
+            self.line_longitude.setText('')
+            self.search()
 
     def addImageToUi(self, image):
         item = ImageListItem(image.filename, image)
