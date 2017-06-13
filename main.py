744354b174c2ff5e86867bf7ee9825f22dbaa284
Andrey <agromov@sfu.ca>
2017-06-12 23:27:45 -0700

    Configured interop library import

--- main.py
+++ main.py
@@ -7,6 +7,14 @@ from gui.mainWindow import MainWindow, TAB_INDICES
 from watcher import Watcher
 from utils.imageInfo import processNewImage
 
+# use this if you want to include modules from a subfolder
+import inspect
+cmd_subfolder = os.path.realpath(
+    os.path.abspath(os.path.join(os.path.split(inspect.getfile(inspect.currentframe()))[0], "../interop/client/")))
+if cmd_subfolder not in sys.path:
+    sys.path.insert(0, cmd_subfolder)
+from interop.client import Client
+
 
 class Controller():
     def __init__(self):
